05bdcec775bbd6f5e725e3f33c760ef6acca7856
Roy Rosenberg <rosenr9@medtronic.com>
2024-08-12 13:37:43 +0300

    Add project files.

---
+++ K8S.WebApp/Controllers/NumberController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace K8S.WebApp.Controllers
+{
+    public class NumberController : Controller
+    {
+        public IActionResult Index(int id = 1)
+        {
+            List<int> res = new List<int>();
+            for (int i = 0; i < id; i++)
+            {
+                res.Add(Random.Shared.Next(0, 100));
+            }
+            return Ok(res);
+        }
+    }
+}
---
+++ K8S.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace K8S.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
